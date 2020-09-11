1e87a717bab606909e17c087c0daeb486d182677
masked-rpgmaker <masked.rpg@gmail.com>
2020-09-11 18:08:23 -0300

    Issue templates & rollup adjustments

--- rollup.config.js
+++ rollup.config.js
@@ -37,7 +37,7 @@ export default [
             }
         ],
         plugins: [
-            babel({ "presets": ["@babel/preset-env"] })
+            babel({ "presets": ["@babel/preset-env"], babelHelpers: 'bundled' })
         ]
 	}
 ];
